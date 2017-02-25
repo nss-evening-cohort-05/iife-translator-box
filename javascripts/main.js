3c09b9c5832c38e6b302d0d5e145feab06f91f0e
bryanhmiller <bryankalos@hotmail.com>
2017-02-25 13:47:14 -0600

    Older version of code keeping me from pulling up to date code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,32 @@
 var Translator = (function(){
 var userInput = document.getElementById("inputLang");
 var translateButton = document.getElementById("translate");
+var languageChoice = document.getElementsByName("language").value;
 
-translateButton.addEventListener("click",functionname);
+function selectedLanguage(){
+	console.log("test",languageChoice);
+	for (var i = 0; i < 4; i++) {
+		
+	}
+
+} 
+
+
+
+
+
 };
 )(Translator || {});
+
+translateButton.addEventListener("click", selectedLanguage);
+// function selectedLanguage() {
+// 	if (languageChoice === "spanish") {
+// 		translateToSpanish();
+// 	} else if (languageChoice === "french") {
+// 		translateToFrench();
+// 	} else if (languageChoice === "vietvaluese") {
+// 		translateToVietnamese();
+// 	} else if (languageChoice === "german") {
+// 		translateToGerman();
+// 	}
+// }
